491b36790839c7ebdcac66300d75931b966d920a
ricotandrio <ricotandrio@gmail.com>
2022-12-11 20:21:52 +0700

    Update ver3

--- mainStack.cpp
+++ mainStack.cpp
@@ -4,7 +4,7 @@
 struct Stack{
     int n;
     Stack *up, *down;
-}*top = NULL, *bottom = NULL;
+}*top = NULL;
 
 Stack *createNode(int n){
     Stack *newNode = (Stack*) malloc(sizeof(Stack));
@@ -14,8 +14,8 @@ Stack *createNode(int n){
 }
 
 void pushTop(Stack *createNode){
-    if(bottom == NULL){
-        top = bottom = createNode;
+    if(top == NULL){
+        top = createNode;
     } else {
         top->up = createNode;
         createNode->down = top;
@@ -23,100 +23,20 @@ void pushTop(Stack *createNode){
     }
 }
 
-void pushBottom(Stack *createNode){
-    if(bottom == NULL){
-        top = bottom = createNode;
-    } else {
-        createNode->up = bottom;
-        bottom->down = createNode;
-        bottom = createNode;
-    }
-}
-
-void pushMid(Stack *createNode){
-    if(bottom == NULL){
-        top = bottom = createNode;
-    }
-    else if(createNode->n <= bottom->n){
-        pushBottom(createNode);
-    }
-    else if(createNode->n >= top->n){
-        pushTop(createNode);
-    }
-    else {
-        Stack *curr = bottom;
-        while(curr->up != NULL){
-            if(createNode->n >= curr->n && createNode->n < curr->up->n){
-                break;
-            }
-            curr = curr->up;
-        }
-        Stack *afterCurr = curr->up;
-        curr->up = createNode;
-        createNode->down = curr;
-
-        createNode->up = afterCurr;
-        afterCurr->down = createNode;
-    }
-}
-
 void popTop(){
-    if(bottom == NULL){
+    if(top == NULL){
         return;
     }
-    else if(bottom == top){
-        bottom->up = top->down = NULL; // DELETE ARROW
-        free(bottom);
+    else if(top->down == NULL){
+        free(top);
+        top = NULL;
     }
     else {
         Stack *newTop = top->down;
         newTop->up = top->down = NULL; // DELETE ARROW
         free(top);
+        top = NULL;
         top = newTop;
-    }
-}
-
-void popBottom(){
-    if(bottom == NULL){
-        return;
-    }
-    else if(bottom == top){
-        bottom->up = top->down = NULL; // DELETE ARROW
-        free(bottom);
-    }
-    else {
-        Stack *newBottom = bottom->up;
-        newBottom->down = bottom->up = NULL; // DELETE ARROW
-        free(bottom);
-        bottom = newBottom;
-    }
-}
-
-void popMid(Stack *createNode){
-    if(bottom == NULL){
-        return;
-    }
-    else if(createNode->n == bottom->n){
-        popBottom();
-    }
-    else if(createNode->n == top->n){
-        popTop();
-    }
-    else {
-        Stack *curr = bottom;
-        while(curr->up != NULL){
-            if(curr->n == createNode->n){
-                break;
-            }
-            curr = curr->up;
-        }
-        if(curr == top) printf("DATA NOT FOUND : %d\n", createNode->n); return;
-        Stack *beforeCurr = curr->down;
-        Stack *afterCurr = curr->up;
-        curr->up = curr->down = NULL; // DELETE ARROW
-        free(curr);
-        beforeCurr->up = afterCurr;
-        afterCurr->down = beforeCurr;
     }
 }
 
@@ -127,7 +47,7 @@ void display(){
         puts("NULL");
         Stack *curr = top;
         while(curr != NULL){
-            printf(" %d\n", curr->n);
+            printf("%d\n", curr->n);
             puts("===");
             curr = curr->down;
         }
@@ -138,29 +58,19 @@ void display(){
 int main(){
     // INSERT
     puts("Display #1:");
-    pushTop(createNode(6));
     pushTop(createNode(8));
-    pushBottom(createNode(5));
-    pushBottom(createNode(4));
-    pushMid(createNode(6));
-    pushMid(createNode(20));
-    pushMid(createNode(12));
-    pushMid(createNode(4));
-    pushMid(createNode(15));
-    pushTop(createNode(25));
-    pushBottom(createNode(1));
+    pushTop(createNode(5));
+    pushTop(createNode(10));
+    pushTop(createNode(3));
+    pushTop(createNode(21));
+    pushTop(createNode(2));
+    pushTop(createNode(15));
     display();
 
     // DELETE
     puts("\nDisplay #2:");
-    popBottom();
-    popMid(createNode(4));
-    popMid(createNode(5));
-    popMid(createNode(6));
-    popMid(createNode(25));
-    popMid(createNode(7));
     popTop();
-    popBottom();
+    popTop();
     display();
     return 0;
 }
